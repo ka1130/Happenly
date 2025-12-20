1e02fae0c5845593d1410bbea716f983a5f40653
Kamila Matla-Tomczyk <ka1130@users.noreply.github.com>
2025-12-20 14:12:02 +0100

    Add form to create an event

--- app/events/new/page.tsx
+++ app/events/new/page.tsx
@@ -1,5 +1,110 @@
 'use client';
 
+import { useState } from 'react';
+
 export default function NewEventPage() {
-  return <h1>Create a new event</h1>;
+  const [form, setForm] = useState({
+    title: '',
+    description: '',
+    date: '',
+    location: '',
+    capacity: 0,
+    registrations: 0,
+    category: '',
+    active: false,
+  });
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    // const res = await
+  };
+
+  return (
+    <>
+      <h1>Create a new event</h1>
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-md mx-auto p-4 bg-white shadow rounded space-y-4"
+      >
+        <label className="block text-sm font-medium text-gray-700">Title</label>
+        <input
+          type="text"
+          value={form.title}
+          onChange={(e) => setForm({ ...form, title: e.target.value })}
+          className="mt-1 block w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <label className="block text-sm font-medium text-gray-700">
+          Description
+        </label>
+        <input
+          type="text"
+          value={form.description}
+          onChange={(e) => setForm({ ...form, description: e.target.value })}
+          className="mt-1 block w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <label className="block text-sm font-medium text-gray-700">Date</label>
+        <input
+          type="date"
+          value={form.date}
+          onChange={(e) => setForm({ ...form, date: e.target.value })}
+          className="mt-1 block w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <label className="block text-sm font-medium text-gray-700">
+          Location
+        </label>
+        <input
+          type="text"
+          value={form.location}
+          onChange={(e) => setForm({ ...form, location: e.target.value })}
+          className="mt-1 block w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <label className="block text-sm font-medium text-gray-700">
+          Capacity
+        </label>
+        <input
+          type="number"
+          value={form.capacity}
+          onChange={(e) =>
+            setForm({ ...form, capacity: Number(e.target.value) })
+          }
+          className="mt-1 block w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <label className="block text-sm font-medium text-gray-700">
+          Registrations
+        </label>
+        <input
+          type="number"
+          value={form.registrations}
+          onChange={(e) =>
+            setForm({ ...form, registrations: Number(e.target.value) })
+          }
+          className="mt-1 block w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <label className="block text-sm font-medium text-gray-700">
+          Category
+        </label>
+        <input
+          type="text"
+          value={form.category}
+          onChange={(e) => setForm({ ...form, category: e.target.value })}
+          className="mt-1 block w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <label className="mt-2 inline-flex items-center space-x-2 text-sm font-medium text-gray-700">
+          <input
+            type="checkbox"
+            checked={form.active}
+            onChange={(e) => setForm({ ...form, active: e.target.checked })}
+            className="h-5 w-5 text-blue-600 border border-gray-300 rounded focus:ring-2 focus:ring-blue-500 appearance-none checked:bg-blue-600 checked:border-blue-600"
+          />
+          <span>Active</span>
+        </label>
+        <button
+          type="submit"
+          className="w-full mt-2  bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition"
+        >
+          Create Event
+        </button>
+      </form>
+    </>
+  );
 }
