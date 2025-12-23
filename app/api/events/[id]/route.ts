5cc6e122290aa2301242f66ab7120f993a0aec4b
Kamila Matla-Tomczyk <ka1130@users.noreply.github.com>
2025-12-23 11:54:32 +0100

    Add patch and delete methods

--- app/api/events/[id]/route.ts
+++ app/api/events/[id]/route.ts
@@ -1,37 +1,44 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { events, Event } from '@data/events';
+import { NextRequest, NextResponse } from "next/server";
+import { events } from "@data/events";
 
-type Context = {
-  params: Promise<{
-    id: string;
-  }>;
+type Params = {
+  id: string;
 };
 
-// export async function GET(req: NextRequest, context: Context) {
-//   //
-//   console.log('context', context);
-//   // const {id}
-
-//   // const event = events.find((e) => e.id === context.params.id);
-
-//   // if (!event) {
-//   //   return NextResponse.json({ error: 'Not found' }, { status: 404 });
-//   // }
-
-//   return NextResponse.json(event);
-// }
-
-export async function GET(
-  _req: NextRequest,
-  ctx: RouteContext<'/api/events/[id]'>
-) {
-  const { id } = await ctx.params;
+export async function GET(_req: NextRequest, { params }: { params: Params }) {
+  const { id } = params;
 
   // TU docelowo będzie baza / serwis
   // const event = await db.event.findById(id)
 
   return NextResponse.json({
     id,
-    status: 'TODO: fetch from database',
+    status: "TODO: fetch from database",
   });
+}
+
+export async function PATCH(req: NextRequest, { params }: { params: Params }) {
+  const { id } = params;
+  const data = await req.json();
+
+  // TODO: update event by id with partial data
+
+  return NextResponse.json({
+    id,
+    updated: data,
+  });
+}
+
+export async function DELETE(
+  _req: NextRequest,
+  { params }: { params: Params },
+) {
+  const { id } = params;
+  const index = events.findIndex((event) => event.id === params.id);
+  if (index === -1)
+    return NextResponse.json({ error: "Not found" }, { status: 404 });
+
+  events.splice(index, 1);
+
+  return NextResponse.json(null, { status: 204 });
 }
