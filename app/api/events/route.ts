bfa189cb15ba2e14261f3d85cd2b62d1a0f967f3
Kamila Matla-Tomczyk <ka1130@users.noreply.github.com>
2025-12-29 15:18:18 +0100

    Add user uuid to seed

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -15,16 +15,25 @@ export async function GET(_req: NextRequest) {
 }
 
 export async function POST(req: NextRequest) {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
   const body = await req.json();
 
+  // dodajemy user_id do eventu
   const { data, error } = await supabase
     .from("events")
-    .insert([body])
+    .insert([{ ...body, user_id: user.id }])
     .select()
     .single();
 
   if (error) {
-    return NextResponse.json({ error: error.message }, { status: 401 });
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 
   return NextResponse.json(data);
--- app/api/seed-events/route.ts
+++ app/api/seed-events/route.ts
@@ -17,6 +17,7 @@ export async function GET(_req: NextRequest) {
       published: true,
       image:
         "https://images.stockcake.com/public/0/6/0/060467c1-2440-4b8a-bcde-802f6f0259f0/vr-art-experience-stockcake.jpg",
+      user_id: "02d96ead-93f7-4342-a2a3-7d8445add5ba",
     },
     {
       title: "React + Next.js Workshop",
@@ -32,6 +33,7 @@ export async function GET(_req: NextRequest) {
       published: false,
       image:
         "https://images.stockcake.com/public/f/8/c/f8cd7c02-92ee-4ad9-8109-ed7b920d4f4e_large/code-in-focus-stockcake.jpg",
+      user_id: "02d96ead-93f7-4342-a2a3-7d8445add5ba",
     },
     {
       title: "Digital Art Expo",
@@ -47,6 +49,7 @@ export async function GET(_req: NextRequest) {
       published: false,
       image:
         "https://images.stockcake.com/public/a/c/1/ac143f91-28ad-4cb8-9387-d149032bbdf5_large/future-on-display-stockcake.jpg",
+      user_id: "02d96ead-93f7-4342-a2a3-7d8445add5ba",
     },
   ];
 
