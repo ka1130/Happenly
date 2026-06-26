0b68abafa66cdef0198d25e96623015bdbb51d40
Kamila Matla <kamila.varia@gmail.com>
2026-06-26 19:58:25 +0200

    Configure crone job

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,5 +1,21 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
+import { supabase } from "@/lib/supabase";
 
-export async function GET() {
-  return NextResponse.json({ ok: true });
+export async function GET(req: Request) {
+  const authHeader = req.headers.get("authorization");
+
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  const { data, error } = await supabase.from("events").select("id").limit(1);
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json({
+    success: true,
+    message: "Supabase successfully pinged!",
+  });
 }
